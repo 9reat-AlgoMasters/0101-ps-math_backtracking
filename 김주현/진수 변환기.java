30c15999b7002c559c15415ca7db0791e0806918
skydreamer21 <skydreamer210@gmail.com>
2023-02-02 22:52:35 +0900

    refactor: 진수 변환기 주석 추가

--- 김주현/진수 변환기.java
+++ 김주현/진수 변환기.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+// 메인 로직 : N을 M으로 나눈 나머지를 순서대로 저장하고 맨 마지막에 저장된 순서를 뒤집어 출력한다.
 public class Q14915 {
     static Map<Integer, Character> digitTable = new HashMap<>();
     
@@ -19,7 +20,7 @@ public class Q14915 {
         if (m == 0) sb.append(0);
         while (m > 0) {
             int rem = m % n;
-//            sb.append(rem < 10 ? rem : digitTable.get(rem)); -> 오류 코드
+//            sb.append(rem < 10 ? rem : digitTable.get(rem)); -> 오류 코드 -> 오류 원인 복습 위해 주석처리
             if (rem >= 10) {
                 sb.append(digitTable.get(rem));
             } else {
